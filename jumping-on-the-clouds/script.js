945f8c6ce45611f5059e2d612608d932be04190a
danailkostov <dankostov359@gmail.com>
2020-09-17 20:49:21 +0300

    jump on the clouds task

---
+++ jumping-on-the-clouds/script.js
@@ -0,0 +1,44 @@
+/*
+
+Emma is playing a new mobile game that starts with consecutively numbered clouds.
+Some of the clouds are thunderheads and others are cumulus.
+She can jump on any cumulus cloud having a number that is equal to the number of the current cloud
+plus 1 or 2. She must avoid the thunderheads. Determine the minimum number of jumps
+it will take Emma to jump from her starting postion to the last cloud.
+It is always possible to win the game.
+
+For each game, Emma will get an array of clouds numbered 0 if they are safe
+or 1 if they must be avoided. For example, c=[0,1,0,0,0,1,0] indexed from 0...6.
+The number on each cloud is its index in the list so she must avoid the clouds at indexes 1 and 5.
+She could follow the following two paths: 0 => 2 => 4 => 6 or 0 => 2 => 3 => 4 => 6.
+The first path takes 3 jumps while the second takes 4.
+
+*/
+
+/*
+
+Task:
+Write function which returns(integer) minimum number of jumps required for winning the game
+
+ */
+function jumpingOnClouds(c) {
+  let turns = 0;
+
+  for (let indexOfC = 0, jump = 2; indexOfC < c.length; indexOfC += jump) {
+    const count = [];
+    for (let i = 0; i < 1; i += 1) {
+      count.push(c[indexOfC + 1]);
+      count.push(c[indexOfC + 2]);
+    }
+    if (count[1] === 1 || count[1] === undefined) {
+      jump = 1;
+    } else {
+      jump = 2;
+    }
+    turns += 1;
+  }
+  return turns - 1;
+}
+
+// console.log(jumpingOnClouds([0, 0, 0, 0, 1, 0])); // Should return - 3
+// console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])); // Should return - 4
